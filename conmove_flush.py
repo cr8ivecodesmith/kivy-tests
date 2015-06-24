f2873cc44ba355cdcc74f81a9a172f608a702522
Matt Lebrun <lebrun.matt@gmail.com>
2015-06-24 16:12:09 +0800

    Console experiment. Flush all and re-render approach.

---
+++ conmove_flush.py
@@ -0,0 +1,108 @@
+import kivy
+kivy.require('1.9.0')
+
+import pdb
+
+from kivy.app import App
+from kivy.base import EventLoop
+from kivy.clock import Clock
+from kivy.core.window import Window
+from kivy.uix.widget import Widget
+
+from console import Console
+
+
+SCREEN_SIZE = (80, 50)
+
+
+class Con(Console):
+    _sprites = []
+
+    def add_sprite(self, sprite):
+        sprite.console = self
+        self._sprites.append(sprite)
+
+    def get_sprite(self, sprite_id):
+        for spr in iter(self._sprites):
+            if spr.id == sprite_id:
+                return spr
+
+    def flush(self):
+        for spr in iter(self._sprites):
+            self.put_char(spr.char, pos=(spr.x, spr.y),
+                          color=spr.color)
+
+        super().flush()
+
+        # Delete everything except the background color
+        del self._vertices[self._vsize * 4:]
+        del self._indices[6:]
+
+
+class Sprite(object):
+    def __init__(self, id, char, x, y, color=None, console=None):
+        self.id = id
+        self.char = char
+        self.x, self.y = x, y
+        self.color = color
+        self.console = console
+
+
+class ConEmu(Widget):
+    def initialize(self):
+        self.add_widget(Con(
+            id='con',
+            screen_size=SCREEN_SIZE,
+            bg_color=(0.3, 0.3, 0.3),
+        ))
+        self.con = self.get_widget_by_id('con')
+        self.con.add_sprite(Sprite(
+            id='npc',
+            char='@',
+            x=SCREEN_SIZE[0] / 2,
+            y=SCREEN_SIZE[1] / 2,
+        ))
+        self.con.add_sprite(Sprite(
+            id='player',
+            char='@',
+            color=(1.0, 1.0, 0.0),
+            x=SCREEN_SIZE[0] / 2 + 5,
+            y=SCREEN_SIZE[1] / 2,
+        ))
+        self.player = self.con.get_sprite('player')
+
+    def update(self, nap):
+        px, py = Window.mouse_pos
+        self.player.x, self.player.y = px, py
+
+        self.con.put_text('nap: {:.4f}'.format(nap), pos=(1, 1))
+        self.con.put_text('player coord: ({},{})'.format(
+            self.player.x, self.player.y), pos=(1, 2))
+        self.con.put_text('screen size: {}'.format(
+            SCREEN_SIZE), pos=(1, 3))
+
+        self.con.flush()
+
+    def get_widget_by_id(self, widget_id, restrict=True, loopback=False):
+        for obj in self.walk(restrict=restrict, loopback=loopback):
+            if obj.id == widget_id:
+                return obj
+
+
+class ConEmuApp(App):
+    def build(self):
+        EventLoop.ensure_window()
+        return ConEmu()
+
+    def on_start(self):
+        self.root.initialize()
+        Clock.schedule_interval(self.root.update, 1 / 25.0)
+
+
+def main():
+    ConEmuApp().run()
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main())
